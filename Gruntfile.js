d1196c7c3c0c3270956dc0b0845f2c43cc7f6a56
unknown <apimentel@brdlapimentel.daitan.com>
2015-04-09 15:18:29 -0300

    New tasks for grunt :orange:

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,53 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            files: ['Gruntfile.js', 'src/main/webapp/**/*.js', 'test/**/*.js', '!src/main/webapp/lib/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/main/webapp/**/*.js'],
+                dest: 'target/classes/<%= pkg.name %>.js'
+            }
+        },
+
         uglify: {
             options: {
-                banner: '/*! ### <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
             },
-            build: {
-                src: 'src/**/*.js',
-                dest: 'target/classes/target.min.js'
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'target/classes/<%= pkg.name %>.js'
             }
+        },
+
+        qunit: {
+            files: ['test/**/*.html']
+        },
+
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
         }
     });
 
-    //grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['uglify']);
-    //grunt.registerTask('default', ['handlebars', 'uglify']);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 };
