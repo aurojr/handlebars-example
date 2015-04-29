ea01294d61f4578cecfd89e28fa0ed97dc4d1666
Auro do N. Pimentel Jr <auro.np@gmail.com>
2015-04-29 16:28:58 -0300

    Moved menu mock to module.

--- src/main/webapp/js/pre-loader.js
+++ src/main/webapp/js/pre-loader.js
@@ -10,7 +10,7 @@ var App = App || {};
 
         loadMenu: function () {
             App.PreLoader.fetchMenuPartial().done(function () {
-                App.API.processTemplate('nav', App.PreLoader.Menu);
+                App.API.processTemplate('nav', App.Menu);
             });
         },
 
@@ -18,23 +18,6 @@ var App = App || {};
             return $.get('./modules/menu/menu.html', function (data) {
                 Handlebars.registerPartial('menu', data);
             }, "html");
-        },
-
-        Menu: {
-            items: [
-                {
-                    label: 'Link1',
-                    href: 'link1'
-                }, {
-                    label: 'Link2',
-                    href: 'link2'
-                }, {
-                    label: 'Link3',
-                    href: 'link3'
-                }, {
-                    label: 'Link4',
-                    href: 'link4'
-                }]
         }
     };
 }(Handlebars, jQuery));
---
+++ src/main/webapp/modules/menu/menu.js
@@ -0,0 +1,19 @@
+var App = App || {};
+
+(function () {
+    App.Menu = {
+        items: [{
+            label: 'Link1',
+            href: 'link1'
+        }, {
+            label: 'Link2',
+            href: 'link2'
+        }, {
+            label: 'Link3',
+            href: 'link3'
+        }, {
+            label: 'Link4',
+            href: 'link4'
+        }]
+    };
+}());
