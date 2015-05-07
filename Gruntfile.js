9d8d41557ba03ce8959998468115fc6822ffccba
Auro Pimentel Jr <auro.np@gmail.com>
2015-05-07 16:54:31 -0300

    Added mocha and chai to support automated tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,109 @@
 'use strict';
 
 module.exports = function (grunt) {
-    // Time how long tasks take. Can help when optimizing build times
-    require('time-grunt')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
 
-    // Configurable paths
-    var config = {
-        app: 'src/main/webapp',
-        dist: 'target/classes'
-    };
+  // Configurable paths
+  var config = {
+    app: 'src/main/webapp',
+    test: 'src/main/test',
+    dist: 'target/classes',
+    bowerTest: grunt.file.readJSON('src/main/test/.bowerrc')
+  };
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        config: config,
+    config: config,
 
-        jshint: {
-            files: [
-                'Gruntfile.js',
-                'src/main/webapp/**/*.js',
-                'test/**/*.js',
-                '!src/main/webapp/lib/**/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        '<%= config.app %>/**/*.js',
+        '<%= config.test %>/**/*.js',
+        '!<%= config.app %>/lib/**/*.js',
+        '!<%= config.test %>/<%= config.bowerTest.directory %>/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
 
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['src/main/webapp/**/*.js'],
-                dest: 'target/classes/<%= pkg.name %>.js'
-            }
-        },
-
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
-            },
-            prod: {
-                src: ['src/main/webapp/**/*.js'],
-                dest: 'target/classes/<%= pkg.name %>.min.js'
-            }
-        },
-
-        wiredep: {
-            app: {
-                ignorePath: /^\/|\.\.\//,
-                src: ['<%= config.app %>/index.html']
-            }
-        },
-
-        qunit: {
-            files: ['test/**/*.html']
-        },
-
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
+    // Mocha testing framework configuration options
+    mocha: {
+      all: {
+        options: {
+          run: true,
+          urls: ['http://<%= connect.options.hostname %>:<%= connect.test.options.port %>/index.html']
         }
-    });
+      }
+    },
 
-    grunt.registerTask('development', ['jshint', 'concat', 'wiredep']);
-    grunt.registerTask('production', ['jshint', 'uglify']);
-    grunt.registerTask('default', ['development']);
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        open: true,
+        livereload: 35729,
+        // Change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      test: {
+        options: {
+          open: false,
+          port: 9001,
+          middleware: function (connect) {
+            return [
+              connect.static(config.test),
+              connect().use('/bower_components', connect.static('<%= config.test %>/bower_components'))
+            ];
+          }
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['<%= config.app %>/**/*.js'],
+        dest: '<%= config.dist %>/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      prod: {
+        src: ['<%= config.app %>/**/*.js'],
+        dest: '<%= config.dist %>/<%= pkg.name %>.min.js'
+      }
+    },
+
+    wiredep: {
+      app: {
+        ignorePath: /^\/|\.\.\//,
+        src: ['<%= config.app %>/index.html']
+      },
+      test: {
+        ignorePath: /^\/|\.\.\//,
+        src: ['<%= config.test %>/index.html']
+      }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.registerTask('development', ['jshint', 'concat', 'wiredep:app', 'test']);
+  grunt.registerTask('test', ['wiredep:test', 'connect:test', 'mocha']);
+  grunt.registerTask('production', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['development']);
 };
